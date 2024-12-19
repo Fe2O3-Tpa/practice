abd610e15b254849e1ed6658d4509404a1d98cc5
Fe2O3-Tpa <gogottu1221@gmail.com>
2024-12-19 19:09:26 +0900

    main.rsにfizzbuzzを追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let fizzbuzz_maxium: u64 = 810;
+    for n in 1..=fizzbuzz_maxium {
+        println!("{}", fizz_buzz(n))
+    }
+}
+
+fn fizz_buzz(n: u64) -> String{
+    let n_string: String = n.to_string();
+    match (n%3,n%5) {
+        (0, 0) => n_string + ": FizzBuzz",
+        (0, _) => n_string + ": Fizz",
+        (_, 0) => n_string + ": Buzz",
+        (_,_) => n_string
+    }
 }
