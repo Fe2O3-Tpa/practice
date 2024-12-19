cfa354cb15de69ce9ad2a6ba504feac7c86146ce
Fe2O3-Tpa <gogottu1221@gmail.com>
2024-12-19 21:23:05 +0900

    fizzbuzに区切り文字機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 fn main() {
     let fizzbuzz_maxium: u64 = 810;
     for n in 1..=fizzbuzz_maxium {
-        println!("{}", fizz_buzz(n))
+        println!("{}", fizz_buzz(n, ": "))
     }
 }
 
-fn fizz_buzz(n: u64) -> String{
+fn fizz_buzz(n: u64, punc: &str) -> String{ // n 調べる数 符号なし64bit整数(u64), punc 区切り文字
     let n_string: String = n.to_string();
     match (n%3,n%5) {
-        (0, 0) => n_string + ": FizzBuzz",
-        (0, _) => n_string + ": Fizz",
-        (_, 0) => n_string + ": Buzz",
+        (0, 0) => n_string + punc + "FizzBuzz",
+        (0, _) => n_string + punc + "Fizz",
+        (_, 0) => n_string + punc + "Buzz",
         (_,_) => n_string
     }
 }
